8cb20ffbe0328afda029d2fcccb8d00a2f96e8a3
sharvin18 <dedhiasharvin@gmail.com>
2021-11-10 15:04:20 +0530

    setup: download page server

---
+++ routes/showFile.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const File = require('../models/file');
+
+router.get('/:uuid', async (req, res) => {
+   
+    // Access the file from database using the uuid
+
+    try{
+
+        const file = await File.findOne({ uuid: req.params.uuid });
+
+        if(!file){
+            return res.render('download', {error: "Link has been expired" });
+        }
+
+        return res.render('download', {
+            uuid: file.uuid,
+            fileName: file.fileName,
+            fileSize: file.fileSize,
+            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+        });
+
+    }catch(err) {
+        return res.render('download', { error: "Something went wrong" });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const connectDb = require('./config/database');
-
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3030;
 
 connectDb();
 
+// Template engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
 // Routes
 app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/showFile'));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
