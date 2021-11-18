dbbab585c9df9d4ac2c9c24ec33d6b8ba0784b48
sharvin18 <dedhiasharvin@gmail.com>
2021-11-18 18:57:07 +0530

    updated base url for deployment

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:3030";
+const baseURL = "https://file-share-1.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/email`;
 
